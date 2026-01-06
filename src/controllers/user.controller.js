632cd4ff042acf07120a7fd3df9fdca5ec2d2a17
Manish Chauhan <chauhanmanish2021dev@gmail.com>
2026-01-06 17:07:41 +0530

    feat: implement user account management features including password change, account details update, and avatar/cover image upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -217,9 +217,123 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+   const { oldPassword, newPassword } = req.body
+
+   const user = await User.findById(req.user._id)
+   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+   if (!isPasswordCorrect) {
+      throw new ApiError(400, "old password is incorrect")
+   }
+
+   user.password = newPassword
+   await user.save({validateBeforeSave: false})
+   
+   return res
+   .status(200)
+   .json(new ApiError(200, "password chanded successfully"))
+
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+   return res
+   .status(200)
+   .json(200, req.user, "current user fetched successfully")
+
+})
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+   const {fullName, email} = req.body
+
+   if (!fullName || !email) {
+      throw new ApiError(400, "fullName and email are required")
+   }
+
+   const user = User.findByIdAndUpdate(
+      req.user._id,
+      {
+         $set: {
+            fullName,
+            email 
+         }
+      },
+      {new: true}
+   ).select("-password")
+
+   return res
+   .status(200)
+   .json(new ApiResponse(200 , user , "user details updated succcessfully"))
+})
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+   const avatarLocalPath = req.files?.path;
+
+   if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is missing")
+
+   }
+
+   const avatar = await uploadPOnCloudinary(avatarLocalPath)
+
+   if (!avatar.url) {
+      throw new ApiError(400, "Error while uploading avatar")
+
+   }
+
+   const user = await user.findByIdAndUpdate(
+      req.user._id,
+      {
+         $set: {
+            avatar: avatar.url
+         }
+      },
+      {new: true}
+   ).select("-password")
+
+    return res
+   .status(200)
+   .json(new ApiResponse(200, user, "Avatar updated successfully"))
+})
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+   const coverImageLocalPath = req.files?.path;
+
+   if (!coverImageLocalPath) {
+      throw new ApiError(400, "Cover image file is missing")
+
+   }
+
+   const coverImage = await uploadPOnCloudinary(coverImageLocalPath)
+
+   if (!coverImage.url) {
+      throw new ApiError(400, "Error while uploading cover image")
+
+   }
+
+    const user = await user.findByIdAndUpdate(
+      req.user._id,
+      {
+         $set: {
+           coverImage: coverImage.url
+         }
+      },
+      {new: true}
+   ).select("-password")
+
+   return res
+   .status(200)
+   .json(new ApiResponse(200, user, "User cover image updated successfully"))
+})
+
 export {
    registerUser,
    loginUser,
    logoutUser,
-   refreshAccessToken
+   refreshAccessToken,
+   changeCurrentPassword,
+   getCurrentUser,
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage
 }
