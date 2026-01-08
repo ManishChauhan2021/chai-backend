eb8c7b64e750990de4f0cd629b08c27120749c02
Manish Chauhan <chauhanmanish2021dev@gmail.com>
2026-01-08 17:55:52 +0530

    feat: enhance user account features with password change confirmation, user details update, and add user channel profile retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -228,42 +228,42 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
    }
 
    user.password = newPassword
-   await user.save({validateBeforeSave: false})
-   
+   await user.save({ validateBeforeSave: false })
+
    return res
-   .status(200)
-   .json(new ApiError(200, "password chanded successfully"))
+      .status(200)
+      .json(new ApiResponse(200, "password chanded successfully"))
 
 })
 
 const getCurrentUser = asyncHandler(async (req, res) => {
    return res
-   .status(200)
-   .json(200, req.user, "current user fetched successfully")
+      .status(200)
+      .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
-   const {fullName, email} = req.body
+   const { fullName, email } = req.body
 
    if (!fullName || !email) {
       throw new ApiError(400, "fullName and email are required")
    }
 
-   const user = User.findByIdAndUpdate(
+   const user = await User.findByIdAndUpdate(
       req.user._id,
       {
          $set: {
             fullName,
-            email 
+            email
          }
       },
-      {new: true}
+      { new: true }
    ).select("-password")
 
    return res
-   .status(200)
-   .json(new ApiResponse(200 , user , "user details updated succcessfully"))
+      .status(200)
+      .json(new ApiResponse(200, user, "user details updated succcessfully"))
 })
 
 const updateUserAvatar = asyncHandler(async (req, res) => {
@@ -288,12 +288,12 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
             avatar: avatar.url
          }
       },
-      {new: true}
+      { new: true }
    ).select("-password")
 
-    return res
-   .status(200)
-   .json(new ApiResponse(200, user, "Avatar updated successfully"))
+   return res
+      .status(200)
+      .json(new ApiResponse(200, user, "Avatar updated successfully"))
 })
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
@@ -311,19 +311,91 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
    }
 
-    const user = await user.findByIdAndUpdate(
+   const user = await user.findByIdAndUpdate(
       req.user._id,
       {
          $set: {
-           coverImage: coverImage.url
+            coverImage: coverImage.url
          }
       },
-      {new: true}
+      { new: true }
    ).select("-password")
 
    return res
+      .status(200)
+      .json(new ApiResponse(200, user, "User cover image updated successfully"))
+})
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+   const { username } = req.params
+
+   if (!username?.trim()) {
+      throw new ApiError(400, "username is missing")
+   }
+
+   const channel = await User.aggregate([
+        {
+           $match: {
+               username: username.toLowerCase()
+           }
+        },
+        {
+             $lookup: {
+                  from: "subscriptions",
+                  localField: "_id",
+                  foreignField: "channel",
+                  as: "subscribers"
+             }
+        },
+        {
+               $lookup: {
+                  from: "subscriptions",
+                  localField: "_id",
+                  foreignField: "subscriber",
+                  as: "subscribedTo"
+               }
+        },
+        {
+            $addFields: {
+               subscribersCount: {
+                  $size: "$subscribers"
+               },
+               channelIsSubscribedToCount: {
+                  $size: "$sucbscribedTo"
+               },
+               isSubscribed: {
+                  $cond: {
+                     if: {
+                        $sin: [req.user?._id, "$subscribers.subscriber"]
+                     },
+                     then: true,
+                     else: false
+                  }
+               }
+            }
+        },
+        {
+            $project: {
+               fullName: 1,
+               username: 1,
+               sub1scribersCount: 1,
+               channelIsSubscribedToCount: 1,
+               isSubscribed: 1,
+               avatar: 1,
+               coverImage: 1,
+               email: 1,
+               createdAt: 1
+            }
+        }
+   ]) 
+   
+   if (!channel?.length) {
+      throw new ApiError(404, "channel does not exist")
+   }
+
+   return res
    .status(200)
-   .json(new ApiResponse(200, user, "User cover image updated successfully"))
+   .json(new ApiResponse(200, channel[0], "channel profile fetched successfully"))
 })
 
 export {
@@ -335,5 +407,6 @@ export {
    getCurrentUser,
    updateAccountDetails,
    updateUserAvatar,
-   updateUserCoverImage
+   updateUserCoverImage,
+   getUserChannelProfile
 }
