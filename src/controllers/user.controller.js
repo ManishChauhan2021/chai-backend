c8046199e18d7e7e7eec1ecfcb4ab0c7c244bb20
Manish Chauhan <chauhanmanish2021dev@gmail.com>
2026-01-20 13:29:28 +0530

    feat: add getWatchHistory feature to retrieve user's watch history with video and owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadPOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshToken = async (userId) => {
    try {
@@ -398,6 +399,54 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
    .json(new ApiResponse(200, channel[0], "channel profile fetched successfully"))
 })
 
+const getWathchHistory = asyncHandler(async (req, res) => {
+   const user = await User.aggregate([
+      {
+         $match: {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+         }
+      },
+      {
+         $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+               {
+                  $lookup: {
+                     from: "users",
+                     localField: "owner",
+                     foreignField: "_id",
+                     as: "owner",
+                     pipeline: [
+                        {
+                           $project: {
+                              fullName: 1,
+                              username: 1,
+                              avatar: 1
+                           }
+                        }
+                     ]
+                  }
+               },
+               {
+                  $addFields: {
+                     owner: {
+                        $first: "$owner"
+                     }
+                  }
+               }
+            ]
+         }
+      }
+   ])
+
+   return res
+   .status(200)
+   .json(new ApiResponse(200, user[0].watchHistory, "watch history fetched successfully"))
+})
+
 export {
    registerUser,
    loginUser,
@@ -408,5 +457,6 @@ export {
    updateAccountDetails,
    updateUserAvatar,
    updateUserCoverImage,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWathchHistory
 }
